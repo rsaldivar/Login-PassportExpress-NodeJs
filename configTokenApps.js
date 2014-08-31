46c7199ca00fa8ee819d4d32f0f9b16ff3354437
Roberto Carlos Saldivar Armendariz <roberto.saldivararm@gmail.com>
2014-08-31 07:58:38 +0000

    Estrucutra Inicial Login

---
+++ configTokenApps.js
@@ -0,0 +1,13 @@
+var config = {
+  twitter: {
+    key: 'QOBcOvZwv4rvF5rLFNXsO5MgN',
+    secret: 'qjBYCnJxZcrgr94M3UAX5fgMdW7XYr31Z6tg6OMGzmG1XzQbFP'
+  },
+
+  facebook: {
+    id: '1517041665174700',
+    secret: '5c4cff2bec49aeeb5c00dccd5e797245'
+  }
+};
+
+module.exports = config;
